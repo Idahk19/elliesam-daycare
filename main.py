b4bdb843861fffbc7124fcd377f26b2bdae1f975
Idahk19 <idahkarwitha9@gmail.com>
2026-08-11 10:14:04 +0300

    Add the logo and the description

--- main.py
+++ main.py
@@ -19,5 +19,56 @@ left_frame = tk.Frame(
 left_frame.pack(side="left", fill="y") # put to the left and fill vertically
 left_frame.pack_propagate(False) # keep it at the specified width and length
 
+logo = tk.Label(
+    left_frame,
+    text="ELLIESAM",
+    font=("Arial", 30, "bold"),
+    bg="#B9E3F5",
+    fg="#315A72"
+)
+
+logo.pack(pady=(80, 10)) # vertical spacing above and below
+
+daycare = tk.Label(
+    left_frame,
+    text="DAYCARE",
+    font=("Arial", 14, "bold"),
+    bg="#B9E3F5",
+    fg="#315A72"
+)
+daycare.pack(pady=(0, 50))
+
+welcome = tk.Label(
+    left_frame,
+    text="Where little hearts\nlearn, grow & shine",
+    font=("Arial", 24, "bold"),
+    bg="#B9E3F5",
+    fg="#315A72",
+    justify="center"
+)
+welcome.pack(pady=(0, 20))
+
+description = tk.Label(
+    left_frame,
+    text="A simple and organized way to manage\n"
+         "children, caregivers, attendance,\n"
+         "activities and more.",
+    font=("Arial", 11),
+    bg="#B9E3F5",
+    fg="#294A5A",
+    justify="center"
+)
+description.pack(pady=(0, 30))
+
+features = tk.Label(
+    left_frame,
+    text="Safe  |  Caring  |  Organized",
+    font=("Arial", 11, "bold"),
+    bg="#B9E3F5",
+    fg="#315A72"
+)
+
+features.pack(pady=(0, 20))
+
 # Keep the application running
 window.mainloop()
