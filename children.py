6b5263a80c2d4dc34a0b960af968daf070000791
Idahk19 <idahkarwitha9@gmail.com>
2026-08-13 13:41:16 +0300

    Add the search filter and the add child button

--- children.py
+++ children.py
@@ -33,3 +33,46 @@ def children(main_area):
         padx=45,
         pady=(0, 25)
     )
+    actions_frame = tk.Frame(
+       main_area,
+       bg="#FFFFFF"
+    )
+
+    actions_frame.pack(
+       fill="x",
+       padx=45,
+       pady=(10, 20)
+    )
+    search_entry = tk.Entry(
+        actions_frame,
+        font=("Arial", 11),
+        text="Search a child",
+        bg="#EAF7FC",
+        fg="#294A5A",
+        relief="flat",
+        bd=0
+    )
+
+    search_entry.pack(
+        side="left",
+        fill="x",
+        expand=True,
+        ipady=10
+    )
+    add_child_button = tk.Button(
+        actions_frame,
+        text="Add Child",
+        font=("Arial", 11, "bold"),
+        bg="#6FB6D6",
+        fg="#FFFFFF",
+        relief="flat",
+        bd=0,
+        cursor="hand2"
+    )
+
+    add_child_button.pack(
+        side="right",
+        padx=(15, 0),
+        ipady=8,
+        ipadx=15
+    )
