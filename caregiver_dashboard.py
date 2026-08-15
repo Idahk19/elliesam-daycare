aa54005cb8ed28703f1bf3af9e24c3cd7cc5a978
Idahk19 <idahkarwitha9@gmail.com>
2026-08-15 12:41:11 +0300

    Create a side bar for the caregiver dashboard, with children page as default view

--- caregiver_dashboard.py
+++ caregiver_dashboard.py
@@ -1,13 +1,142 @@
 import tkinter as tk
+from children import children
+
 
 def caregiver_dashboard(window):
+
     for item in window.winfo_children():
         item.destroy()
 
     window.title("Ellisam Daycare - Caregiver Dashboard")
     window.configure(bg="#FFFFFF")
+
     main_area = tk.Frame(
-    window,
-    bg="#FFFFFF"
-)
-     
+        window,
+        bg="#FFFFFF"
+    )
+
+    main_area.pack(
+        side="right",
+        fill="both",
+        expand=True
+    )
+
+    sidebar = tk.Frame(
+        window,
+        bg="#B9E3F5",
+        width=230
+    )
+
+    sidebar.pack(
+        side="left",
+        fill="y"
+    )
+
+    sidebar.pack_propagate(False)
+
+    brand = tk.Label(
+        sidebar,
+        text="ELLIESAM",
+        font=("Arial", 22, "bold"),
+        bg="#B9E3F5",
+        fg="#315A72"
+    )
+
+    brand.pack(
+        pady=(40, 0)
+    )
+
+    daycare = tk.Label(
+        sidebar,
+        text="DAYCARE",
+        font=("Arial", 11, "bold"),
+        bg="#B9E3F5",
+        fg="#315A72"
+    )
+
+    daycare.pack(
+        pady=(0, 45)
+    )
+
+    children_button = tk.Button(
+        sidebar,
+        text="Children",
+        font=("Arial", 11, "bold"),
+        bg="#FFFFFF",
+        fg="#315A72",
+        relief="flat",
+        bd=0,
+        anchor="w",
+        padx=25,
+        cursor="hand2",
+        command=lambda: children(main_area, window)
+    )
+
+    children_button.pack(
+        fill="x",
+        padx=15,
+        pady=5
+    )
+
+    attendance_button = tk.Button(
+        sidebar,
+        text="Attendance",
+        font=("Arial", 11),
+        bg="#B9E3F5",
+        fg="#294A5A",
+        relief="flat",
+        bd=0,
+        anchor="w",
+        padx=25,
+        cursor="hand2"
+    )
+
+    attendance_button.pack(
+        fill="x",
+        padx=15,
+        pady=5
+    )
+
+    activities_button = tk.Button(
+        sidebar,
+        text="Activities",
+        font=("Arial", 11),
+        bg="#B9E3F5",
+        fg="#294A5A",
+        relief="flat",
+        bd=0,
+        anchor="w",
+        padx=25,
+        cursor="hand2"
+    )
+
+    activities_button.pack(
+        fill="x",
+        padx=15,
+        pady=5
+    )
+
+    logout_button = tk.Button(
+        sidebar,
+        text="Logout",
+        font=("Arial", 11, "bold"),
+        bg="#B9E3F5",
+        fg="#315A72",
+        relief="flat",
+        bd=0,
+        anchor="w",
+        padx=25,
+        cursor="hand2"
+    )
+
+    logout_button.pack(
+        side="bottom",
+        fill="x",
+        padx=15,
+        pady=25
+    )
+
+    children(
+        main_area,
+        window
+    )
